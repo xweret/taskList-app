c39a15cf50ae848a490ed70033cfc1ad82979a7a
Gabriel Garofalo <gabriel695681@gmail.com>
2022-04-19 18:50:25 -0300

    + +

---
+++ script.js
@@ -0,0 +1,5 @@
+// Info about date 
+const dateNumber = document.getElementById('dateNumber')
+const dateText = document.getElementById('dateText')
+const dateMonth = document.getElementById('dateMonth')
+const dateYear = document.getElementById('dateYear')
