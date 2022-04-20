79723ed39178a33bc59438ac09704e2a7c925351
Gabriel Garofalo <gabriel695681@gmail.com>
2022-04-20 10:23:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const dateNumber = document.getElementById('dateNumber')
 const dateText = document.getElementById('dateText')
 const dateMonth = document.getElementById('dateMonth')
 const dateYear = document.getElementById('dateYear')
+
+// Tasks container 
+
+const tasksContainer = document/getElementById('tasksContainer')
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleDateString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleDateString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleDateString ('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleDateString ('es', { dateYear: 'numeric' });
+};
+
+setDate();
